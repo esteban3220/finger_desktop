0549ebe9cf70c94b688cf9c2afbf5736c753a263
esteban3220 <45909876+esteban3220@users.noreply.github.com>
2022-05-21 19:20:35 -0500

    Main
    - Se añadieron nuevos campo
    - La parte de reconocimiento y subido de huella desde bases de datos ya esta habilitada
    - Se quitaron las entidades (YA NO SE OCUPARA ENCAPSULACION POR FALTA DE TIEMPO)
    - Se corrigieron algunos BUGS

---
+++ src/com/digitalpersona/onetouch/ui/swing/sample/Enrollment/Forma_Captura.java
@@ -0,0 +1,206 @@
+package com.digitalpersona.onetouch.ui.swing.sample.Enrollment;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import com.digitalpersona.onetouch.*;
+import com.digitalpersona.onetouch.capture.*;
+import com.digitalpersona.onetouch.capture.event.*;
+import com.digitalpersona.onetouch.processing.*;
+
+public class Forma_Captura extends JDialog {
+
+    private DPFPCapture capturer = DPFPGlobal.getCaptureFactory().createCapture();
+    private JLabel picture = new JLabel();
+    private JTextField prompt = new JTextField();
+    private JTextArea log = new JTextArea();
+    private JTextField status = new JTextField("[status line]");
+
+    public Forma_Captura(Frame owner) {
+        super(owner, true);
+        setTitle("Inscripción de huellas dactilares");
+
+        setLayout(new BorderLayout());
+        rootPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        picture.setPreferredSize(new Dimension(240, 280));
+        picture.setBorder(BorderFactory.createLoweredBevelBorder());
+        prompt.setFont(UIManager.getFont("Panel.font"));
+        prompt.setEditable(false);
+        prompt.setColumns(40);
+        prompt.setMaximumSize(prompt.getPreferredSize());
+        prompt.setBorder(
+                BorderFactory.createCompoundBorder(
+                        BorderFactory.createTitledBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0), "Prompt:"),
+                        BorderFactory.createLoweredBevelBorder()
+                ));
+        log.setColumns(40);
+        log.setEditable(false);
+        log.setFont(UIManager.getFont("Panel.font"));
+        JScrollPane logpane = new JScrollPane(log);
+        logpane.setBorder(
+                BorderFactory.createCompoundBorder(
+                        BorderFactory.createTitledBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0), "Status:"),
+                        BorderFactory.createLoweredBevelBorder()
+                ));
+
+        status.setEditable(false);
+        status.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        status.setFont(UIManager.getFont("Panel.font"));
+
+        JButton quit = new JButton("Close");
+        quit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+            }
+        });
+
+        JPanel right = new JPanel(new BorderLayout());
+        right.setBackground(Color.getColor("control"));
+        right.add(prompt, BorderLayout.PAGE_START);
+        right.add(logpane, BorderLayout.CENTER);
+
+        JPanel center = new JPanel(new BorderLayout());
+        center.setBackground(Color.getColor("control"));
+        center.add(right, BorderLayout.CENTER);
+        center.add(picture, BorderLayout.LINE_START);
+        center.add(status, BorderLayout.PAGE_END);
+
+        JPanel bottom = new JPanel(new FlowLayout(FlowLayout.TRAILING));
+        bottom.setBackground(Color.getColor("control"));
+        bottom.add(quit);
+
+        setLayout(new BorderLayout());
+        add(center, BorderLayout.CENTER);
+        add(bottom, BorderLayout.PAGE_END);
+
+        this.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentShown(ComponentEvent e) {
+                init();
+                start();
+            }
+
+            @Override
+            public void componentHidden(ComponentEvent e) {
+                stop();
+            }
+
+        });
+
+        pack();
+        setLocationRelativeTo(null);
+    }
+
+    protected void init() {
+        capturer.addDataListener(new DPFPDataAdapter() {
+            @Override
+            public void dataAcquired(final DPFPDataEvent e) {
+                SwingUtilities.invokeLater(new Runnable() {
+                    public void run() {
+                        makeReport("La muestra de huellas dactilares fue capturada.");
+                        setPrompt("Escanea la misma huella dactilar de nuevo.");
+                        process(e.getSample());
+                    }
+                });
+            }
+        });
+        capturer.addReaderStatusListener(new DPFPReaderStatusAdapter() {
+            @Override
+            public void readerConnected(final DPFPReaderStatusEvent e) {
+                SwingUtilities.invokeLater(new Runnable() {
+                    public void run() {
+                        makeReport("La lectora de huellas digitales esta conectada :) ");
+                    }
+                });
+            }
+
+            @Override
+            public void readerDisconnected(final DPFPReaderStatusEvent e) {
+                SwingUtilities.invokeLater(new Runnable() {
+                    public void run() {
+                        makeReport("La lectora de huellas digitales esta desconectada :(");
+                    }
+                });
+            }
+        });
+        capturer.addSensorListener(new DPFPSensorAdapter() {
+            @Override
+            public void fingerTouched(final DPFPSensorEvent e) {
+                SwingUtilities.invokeLater(new Runnable() {
+                    public void run() {
+                        makeReport("La lectora de huellas digitales fue tocada.");
+                    }
+                });
+            }
+
+            @Override
+            public void fingerGone(final DPFPSensorEvent e) {
+                SwingUtilities.invokeLater(new Runnable() {
+                    public void run() {
+                        makeReport("El dedo fue retirado del lector de huellas dactilares..");
+                    }
+                });
+            }
+        });
+        capturer.addImageQualityListener(new DPFPImageQualityAdapter() {
+            @Override
+            public void onImageQuality(final DPFPImageQualityEvent e) {
+                SwingUtilities.invokeLater(new Runnable() {
+                    public void run() {
+                        if (e.getFeedback().equals(DPFPCaptureFeedback.CAPTURE_FEEDBACK_GOOD)) {
+                            makeReport("La calidad de la muestra de huellas dactilares es buena.");
+                        } else {
+                            makeReport("La calidad de la muestra de huellas dactilares es mala.");
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    // Crea la imagen
+    protected void process(DPFPSample sample) {
+        drawPicture(convertSampleToBitmap(sample));
+    }
+
+    protected void start() {
+        capturer.startCapture();
+        setPrompt("Usando el lector de huellas dactilares, escanee su huella dactilar.");
+    }
+
+    protected void stop() {
+        capturer.stopCapture();
+    }
+
+    public void setStatus(String string) {
+        status.setText(string);
+    }
+
+    public void setPrompt(String string) {
+        prompt.setText(string);
+    }
+
+    public void makeReport(String string) {
+        log.append(string + "\n");
+    }
+
+    //Crea imagen de la huella dactilar
+    public void drawPicture(Image image) {
+        picture.setIcon(new ImageIcon(image.getScaledInstance(picture.getWidth(), picture.getHeight(), Image.SCALE_DEFAULT)));
+    }
+
+    protected Image convertSampleToBitmap(DPFPSample sample) {
+        return DPFPGlobal.getSampleConversionFactory().createImage(sample);
+    }
+
+    protected DPFPFeatureSet extractFeatures(DPFPSample sample, DPFPDataPurpose purpose) {
+        DPFPFeatureExtraction extractor = DPFPGlobal.getFeatureExtractionFactory().createFeatureExtraction();
+        try {
+            return extractor.createFeatureSet(sample, purpose);
+        } catch (DPFPImageQualityException e) {
+            return null;
+        }
+    }
+
+}
--- src/conexion/ConexionDB.java
+++ src/conexion/ConexionDB.java
@@ -5,9 +5,17 @@
  */
 package conexion;
 
-import java.sql.Connection;
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -18,20 +26,43 @@ public class ConexionDB {
 
     static String URL ="jdbc:mysql://localhost:3306/banco";
     static String USUARIO = "root";
-    static String CONTRASEÑA = "";
+    static String CONTRASEÑA = "junomava3842";
     Connection con;
     
-    public ConexionDB(){
+    public Connection ConexionDB(){
         
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection(URL, USUARIO, CONTRASEÑA);
+            con = (Connection) DriverManager.getConnection(URL, USUARIO, CONTRASEÑA);
             System.out.println("Conexion establecida...");
+            
         } catch (ClassNotFoundException | SQLException e) {
             System.err.println("No se pudeo establecer la conexion: "+e);
         }    
-    }
-    public Connection getConnection(){
         return con;
+    }
+    
+    
+    public ResultSet ejecuta(String sql,String a,String b,String c,String d,String e,String f,String g,String filename) {
+        ResultSet resultado = null;
+        try {
+            PreparedStatement s =  ConexionDB().prepareStatement(sql);
+            //s.executeUpdate(sql
+            File file = new File(filename);
+            FileInputStream input = new FileInputStream(file);
+            s.setString(1, a);
+            s.setString(2, b);
+            s.setString(3, c);
+            s.setString(4, d);
+            s.setString(5, e);
+            s.setString(6, f);
+            s.setString(7, g);
+            s.setBinaryStream(8, input);
+            s.executeUpdate();
+        } catch (SQLException | FileNotFoundException z) {
+            System.out.println("error:"+z);
+            return null;
+        }
+        return resultado;
     }
 }
